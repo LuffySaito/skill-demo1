ad120aa47788a2bb3d670da50d6abafb4ba74618
LuffySaito <rsaito@ucsd.edu>
2022-04-29 20:32:05 -0700

    test fixed

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test
     public void testMultiplication(){
-        assertEquals(5, SkillDemo1.multiplication(2, 3)); //initially fails
+        assertEquals(6, SkillDemo1.multiplication(2, 3)); //now expect 6, the correct answer
     }
 }
